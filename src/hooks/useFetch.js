52713fd00f9cc8c41409d45186c4744ba8ca6b53
jon-cron <jonathanacm95@gmail.com>
2023-03-04 12:21:13 -0700

    push trouble shooting

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
-import { useState, useEf } from "react";
+import { useState, useEffect } from "react";
 
-export default useFetch = () => {
+export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
