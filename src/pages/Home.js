9fec7b8a6e782c1c05950189c409bef4b19ec275
jon-cron <jonathanacm95@gmail.com>
2023-03-04 12:21:05 -0700

    router link not working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import { useFetch } from "../hooks/useFetch.js";
+import { Link } from "react-router-dom";
+import "./Home.css";
 export default function Home() {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/articles");
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && <div>Is Loading</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+            <Link to={{ pathname: `/articles/${article.id}` }}>
+              Read More...
+            </Link>
+          </div>
+        ))}
     </div>
   );
 }
