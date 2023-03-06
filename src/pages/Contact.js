2ff3e657e8557a022721f8b77b62b8e8eff2e912
jon-cron <jonathanacm95@gmail.com>
2023-03-06 11:24:17 -0700

    router project finished

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Contact() {
+  const queryString = useLocation().search;
+  console.log(queryString);
+  // NOTE this is basic javascript that we are using here
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
     </div>
   );
 }
