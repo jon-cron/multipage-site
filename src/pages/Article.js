69bc2d78baa9999fcf2bb796c563ef65b873710b
jon-cron <jonathanacm95@gmail.com>
2023-03-06 10:37:13 -0700

    fetching data with hook

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch.js";
 
 export default function Article() {
-  return <div>Article</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/articles/" + id;
+  const { data: article, isPending, error } = useFetch(url);
+  return (
+    <div>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {article && (
+        <div>
+          <h2>{article.title}</h2>
+          <p>{article.author}</p>
+          <p>{article.body}</p>
+        </div>
+      )}
+    </div>
+  );
 }
