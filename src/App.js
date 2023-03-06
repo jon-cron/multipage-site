14e2c2992f37cea2b138bafdac170e42a80ea77a
jon-cron <jonathanacm95@gmail.com>
2023-03-06 10:27:49 -0700

    work around for routing window.location.replace

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/articles/:id" element={<Article />} />
+          <Route exact path="/articles/:id" element={<Article />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFetch } from "../hooks/useFetch.js";
-import { Link, BrowserRouter } from "react-router-dom";
+import { NavLink, BrowserRouter } from "react-router-dom";
 import "./Home.css";
 export default function Home() {
   const {
@@ -9,20 +9,29 @@ export default function Home() {
     error,
   } = useFetch("http://localhost:3000/articles");
   return (
-    <div className="home">
-      <h2>Articles</h2>
-      {isPending && <div>Is Loading</div>}
-      {error && <div>{error}</div>}
-      {articles &&
-        articles.map((article) => (
-          <div key={article.id} className="card">
-            <h3>{article.title}</h3>
-            <p>{article.author}</p>
-            <BrowserRouter>
-              <Link to={`/articles/${article.id}`}>Read More...</Link>
-            </BrowserRouter>
-          </div>
-        ))}
-    </div>
+    <BrowserRouter>
+      <div className="home">
+        <h2>Articles</h2>
+        {isPending && <div>Is Loading</div>}
+        {error && <div>{error}</div>}
+        {articles &&
+          articles.map((article) => (
+            <div key={article.id} className="card">
+              <h3>{article.title}</h3>
+              <p>{article.author}</p>
+              <NavLink
+                onClick={() =>
+                  window.location.replace(
+                    `http://localhost:3001/articles/${article.id}`
+                  )
+                }
+                to={`/articles/${article.id}`}
+              >
+                Read More...
+              </NavLink>
+            </div>
+          ))}
+      </div>
+    </BrowserRouter>
   );
 }
